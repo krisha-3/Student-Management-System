b3b716c4b7d7a5e8a396b9f7f3eaa0b8fd1e18a5
krisha-3 <kri385385@gmail.com>
2026-06-09 16:01:18 +0530

    Connect MongoDB

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/student_management");
+
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB Connection Failed");
+    console.error(error.message);
+
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,32 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+connectDB();
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Backend Running");
+});
+
+app.get("/students", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      name: "Krisha",
+      rollNo: "23BECE30153",
+    },
+  ]);
+});
+
+app.post("/students", (req, res) => {
+  console.log(req.body);
+
+  res.json({
+    message: "Student Added Successfully",
+  });
 });
 
 app.listen(5000, () => {
